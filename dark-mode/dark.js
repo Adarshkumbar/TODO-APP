2487435493ac4425b1c63ee8211fde7369c6b420
Adarsh-Kumbar-in <adarshkumbarbgm@gmail.com>
2023-05-13 23:26:02 +0530

    Added dark-mode

---
+++ dark-mode/dark.js
@@ -0,0 +1,9 @@
+// let dark_btn = document.querySelector('img');
+// // console.log(dark_btn);
+// dark_btn.addEventListener("click",()=>{
+//     dark_btn.setAttribute("class","dark-mode")
+// })
+theme=()=>{
+    var btn=document.body;
+    btn.classList.toggle("dark-mode");
+}
